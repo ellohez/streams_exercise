f159faa4e1754d739c1b409d064cf641fa38e8cc
ellohez <helenpjobs@gmail.com>
2023-03-24 10:18:27 +0000

    Removed method ref for 1st exercise to print "Hello "

--- src/com/qa/streams/Runner.java
+++ src/com/qa/streams/Runner.java
@@ -6,25 +6,25 @@ import java.util.stream.Collectors;
 public class Runner {
 
 	public static void main(String[] args) {
-		
+
 		List<String> names = List.of("Michael", "Dean", "James", "Chris");
 		names.stream().forEach(x -> System.out.println("Hello " + x));
-		 
+
 //		The above could actually be done without converting to a stream
 //		as below
 		names.forEach(x -> System.out.println(x));
 //		or using a method reference:
 		names.stream().forEach(System.out::println);
-		
+
 		System.out.printf("%nFiltered: %n");
-		List<String> result = names.stream().filter(str -> !str.equals("James"))
-				.collect(Collectors.toList());
-		result.forEach(System.out::println); 
-		
+		List<String> result = names.stream().filter(str -> !str.equals("James")).collect(Collectors.toList());
+		result.forEach(System.out::println);
+
 //		Combine filter and for each?
 		System.out.printf("%nFiltered and combined: %n");
-		names.stream().filter(str -> !str.equals("James")).forEach(System.out::println);
-		
+//		Can't use a method reference as we need to add "Hello "
+		names.stream().filter(str -> !str.equals("James")).forEach(x -> System.out.println("Hello " + x));
+
 //		Given the following List of Integers, using a stream, 
 //		have it reduce and print the product (all numbers multiplied together).
 		List<Integer> nums = List.of(3, 4, 7, 8, 12);
@@ -33,20 +33,20 @@ public class Runner {
 //		of the reduction operation, and the next element in the stream.
 //		(accumulator, next) ->
 		System.out.printf("%nReduce to product:%n");
-		System.out.println(nums.stream().reduce((a,b) -> a*b).get());
+		System.out.println(nums.stream().reduce((a, b) -> a * b).get());
 //		get() is necessary to return the optional return
-		
+
 //		Starting with a list of numbers, use streams to do the following:
 //		Find the max value.
 		System.out.printf("%nReduce to max:%n");
-		System.out.println(nums.stream().reduce((a,b) -> Math.max(a, b)).get());
+		System.out.println(nums.stream().reduce((a, b) -> Math.max(a, b)).get());
 //		Using a method reference as we have (a,b) on both sides of the lambda accumulator
 		System.out.printf("%nReduce to max (method ref):%n");
 		System.out.println(nums.stream().reduce(Math::max).get());
-		
+
 //		Find the min value
 		System.out.printf("%nReduce to min:%n");
-		System.out.println(nums.stream().reduce((a,b) -> Math.min(a, b)).get());
+		System.out.println(nums.stream().reduce((a, b) -> Math.min(a, b)).get());
 //		Remove the odd numbers
 		System.out.printf("%nRemove the odd numbers:%n");
 		nums.stream().filter(a -> a % 2 == 0).forEach(System.out::println);
@@ -55,16 +55,16 @@ public class Runner {
 		nums.stream().filter(a -> a % 2 != 0).forEach(System.out::println);
 //		Sum the list
 		System.out.printf("%nSum of the list:%n");
-		System.out.println(nums.stream().reduce((a,b) -> a+b).get());
-		
+		System.out.println(nums.stream().reduce((a, b) -> a + b).get());
+
 //		Square every number in the list then remove the 
 //		even numbers and then find the min value
 		System.out.printf("%nSquared numbers:%n");
-		List<Integer> sqNums = nums.stream().map(x -> x*x).collect(Collectors.toList());
+		List<Integer> sqNums = nums.stream().map(x -> x * x).collect(Collectors.toList());
 		sqNums.forEach(System.out::println);
 		System.out.printf("%nSquared numbers without evens:%n");
-		nums.stream().map(x -> x*x).filter(a -> a % 2 != 0).forEach(System.out::println);
+		nums.stream().map(x -> x * x).filter(a -> a % 2 != 0).forEach(System.out::println);
 		System.out.printf("%nSquared numbers without evens, reduced to min:%n");
-		System.out.println(nums.stream().map(x -> x*x).filter(a -> a % 2 != 0).reduce(Math::min).get());
+		System.out.println(nums.stream().map(x -> x * x).filter(a -> a % 2 != 0).reduce(Math::min).get());
 	}
 }
